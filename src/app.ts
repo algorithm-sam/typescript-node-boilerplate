46472590d81bad5f3b58083dd9cb45e0eb755ac0
unknown <oluwatobisamuel2020@gmail.com>
2019-03-22 01:53:45 +0000

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+// import mongoose from "mongoose";
+ import { BASE_PATH } from "./config";
+
+class App {
+
+  public express  = express();
+  public basePath = BASE_PATH || ""
+  constructor(){
+    this.boot();
+  }
+ 
+  private boot(){
+    this.registerMiddleWares();
+    this.mountRoots();
+    this.initializeDB();
+    this.handleUncaughtErrorEvents();
+  }
+  private mountRoots(){}
+  private registerMiddleWares(){}
+  private initializeDB(){}
+  private handleUncaughtErrorEvents(){}
+}
+
+
+export default new App().express;
---
+++ src/config/config.ts
@@ -0,0 +1,4 @@
+import { config } from "dotenv";
+
+export const BASE_PATH = "";
+export const MONGO_URI = "";
